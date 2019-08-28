35d406f95569e64423402895045f3ff42d1970d8
Reginald Ross <rross@averoinc.com>
2019-08-27 22:30:03 -0600

    Initializing Project using component library

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@rbross07/component-lib/dest/static/css/main.caa19371.css';
+import { Content, ProductSection } from "@rbross07/component-lib";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Content > Some Content</Content>
+       <ProductSection products={[{name: 'string'}, {name: 'some-other-string'}]}/>
       </header>
     </div>
   );
-}
+};
 
 export default App;
